fd39c6878b1454e13c512c64cdc1b2094c35c7c6
choish19 <100904838+choish19@users.noreply.github.com>
2024-02-20 01:26:44 +0900

    [Improvement] DTO Schema 정의

--- src/main/kotlin/com/example/demo/web/dto/RequestSketchDto.kt
+++ src/main/kotlin/com/example/demo/web/dto/RequestSketchDto.kt
@@ -1,6 +1,10 @@
 package com.example.demo.web.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class RequestSketchDto (
+    @Schema(description = "스케치 ID")
     var sketchId:String,
+    @Schema(description = "스케치 내 블록 리스트")
     var blockList:List<Block>
 )
--- src/main/kotlin/com/example/demo/web/dto/ResponseSketchDto.kt
+++ src/main/kotlin/com/example/demo/web/dto/ResponseSketchDto.kt
@@ -1,7 +1,12 @@
 package com.example.demo.web.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class ResponseSketchDto (
+    @Schema(description = "스케치 ID")
     var sketchId:String,
+    @Schema(description = "스케치 내 블록 리스트")
     var blockList:List<Block>,
+    @Schema(description = "블록 Output 리스트")
     var blockOutput:List<BlockOutput>
 )
