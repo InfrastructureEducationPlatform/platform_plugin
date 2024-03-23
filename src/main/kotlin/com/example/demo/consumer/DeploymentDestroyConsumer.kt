d70bda20a530860d94526a78e35fc7b36f27a3d0
KangDroid <hyunwoo286@gmail.com>
2024-03-23 14:46:06 +0900

    [Fix] Destroy Consumer에서 Runcatching전체 감싸기

--- src/main/kotlin/com/example/demo/consumer/DeploymentDestroyConsumer.kt
+++ src/main/kotlin/com/example/demo/consumer/DeploymentDestroyConsumer.kt
@@ -38,37 +38,39 @@ class DeploymentDestroyConsumer(
                     exchange = Exchange(value = "deployment.destroy", type = "fanout"),
             )])
     suspend fun consumeDeploymentStartMessage(message: String): Mono<Unit> {
-        val rootMessage = jacksonObjectMapper.readTree(message)
+        runCatching {
+            val rootMessage = jacksonObjectMapper.readTree(message)
 
-        // Get message object field and convert it to a StartDeploymentEvent object
-        val startDeploymentEvent = jacksonObjectMapper.convertValue(rootMessage.get("message"), StartDeploymentEvent::class.java)
+            // Get message object field and convert it to a StartDeploymentEvent object
+            val startDeploymentEvent = jacksonObjectMapper.convertValue(rootMessage.get("message"), StartDeploymentEvent::class.java)
 
-        // Create AWS Credential
-        // {"Region": "Default Region Code(i.e: ap-northeast-2)", "AccessKey": "Access Key ID", "SecretKey": "Access Secret Key"}
-        val awsCredential = AwsConfiguration(
-                region = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["Region"].asText(),
-                accessKeyId = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["AccessKey"].asText(),
-                secretAccessKey = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["SecretKey"].asText()
-        )
+            // Create AWS Credential
+            // {"Region": "Default Region Code(i.e: ap-northeast-2)", "AccessKey": "Access Key ID", "SecretKey": "Access Secret Key"}
+            val awsCredential = AwsConfiguration(
+                    region = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["Region"].asText(),
+                    accessKeyId = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["AccessKey"].asText(),
+                    secretAccessKey = startDeploymentEvent.pluginInstallationProjection.pluginConfiguration["SecretKey"].asText()
+            )
 
 
 
-        //Service 가능 여부 체크
-        startDeploymentEvent.sketchProjection.blockSketch.get("blockList").forEach {
-            val block = jacksonObjectMapper.convertValue(it, Block::class.java)
-            when (it.get("type").asText()) {
-                "virtualMachine" -> vmApiService.isValidVmBlock(block)
-                "webServer" -> webApiService.isValidWebBlock(block)
-                "database" -> dbApiService.isValidDbBlock(block)
-                else -> {
-                    throw CustomException(ErrorCode.INVALID_BLOCK_TYPE)
+            //Service 가능 여부 체크
+            startDeploymentEvent.sketchProjection.blockSketch.get("blockList").forEach {
+                val block = jacksonObjectMapper.convertValue(it, Block::class.java)
+                when (it.get("type").asText()) {
+                    "virtualMachine" -> vmApiService.isValidVmBlock(block)
+                    "webServer" -> webApiService.isValidWebBlock(block)
+                    "database" -> dbApiService.isValidDbBlock(block)
+                    else -> {
+                        throw CustomException(ErrorCode.INVALID_BLOCK_TYPE)
+                    }
                 }
             }
-        }
 
-        //배포 삭제
-        val sketch = jacksonObjectMapper.convertValue(startDeploymentEvent.sketchProjection, RequestSketchDto::class.java)
-        githubFeignService.dispatchGithubAction("destroy-infrastructure", startDeploymentEvent.deploymentLogId, sketch)
+            //배포 삭제
+            val sketch = jacksonObjectMapper.convertValue(startDeploymentEvent.sketchProjection, RequestSketchDto::class.java)
+            githubFeignService.dispatchGithubAction("destroy-infrastructure", startDeploymentEvent.deploymentLogId, sketch)
+        }.recoverCatching {  }
 
         return Mono.empty()
     }
